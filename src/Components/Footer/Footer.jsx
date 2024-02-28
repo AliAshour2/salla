21977afecc17c959b3604db0d94203e3cb58ddca
AliAshour2 <aliashour468@gmail.com>
2024-02-28 20:03:33 +0200

    make nav and footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
-import styles from './Footer.module.css'
+import React from "react";
+import styles from "./Footer.module.css";
+import amazon from "../assets/images/amazon-pay.png"
+import paypal from "../assets/images/logo.png"
+import master from "../assets/images/money.png"
+
+
 function Footer() {
   return (
-    <h2>Footer</h2>
-  )
+    <footer className="bg-main-light fixed-bottom p-3">
+      <div className="container">
+        <h2>Get Salla App</h2>
+        <p>We will send you email with link to the app</p>
+
+        <form>
+          <div className="mb-3 w-75">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter Your Email"
+            />
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
+            </div>
+          </div>
+          
+          
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+
+        <div className="payment-list py-2">
+          
+          <div className={`${styles.paymentBorder} `}>
+          <h5 className="d-inline">Payment Parterns</h5>
+              <img className={`${styles.paymentIcon} ${styles.ml}`} src={amazon} alt="" />
+              <img className={`${styles.paymentIcon} ${styles.ml}`} src={paypal} alt="" />
+              <img className={`${styles.paymentIcon} ${styles.ml}`} src={master} alt="" />
+          </div>
+        </div>
+        <p className=" text-sm-center m-0 ">This Site Created By Ali</p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,110 @@
-import React from 'react'
-import styles from './NavBar.module.css'
+import React from "react";
+import styles from "./NavBar.module.css";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/cart.png";
+import Categories from "./../Categories/Categories";
 function NavBar() {
   return (
-    <h2>NavBar</h2>
-  )
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container">
+          <Link className="navbar-brand" href="#">
+            <img className="w-25" src={logo} alt="" /> Salla
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to={"home"}
+                >
+                  Home
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to={"cart"}
+                >
+                  Cart
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to={"products"}
+                >
+                  products
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to={"Brands"}
+                >
+                  Brands
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to={"categories"}
+                >
+                  Categories
+                </Link>
+              </li>
+            </ul>
+
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item align-self-center">
+                <i className="fa-brands fa-instagram mx-1"></i>
+                <i className="fa-brands fa-facebook mx-1"></i>
+                <i class="fa-brands fa-linkedin mx-1"></i>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to={"register"}>
+                  Rigester
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to={"login"}>
+                  Login
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to={"logout"}>
+                  LogOut
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default NavBar
+export default NavBar;
