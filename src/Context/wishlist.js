794a6d926a34b1ae1ce9b829cb754fe75052c6dc
AliAshour2 <aliashour468@gmail.com>
2024-03-15 02:40:44 +0200

    create wishlist context

--- src/Context/wishlist.js
+++ src/Context/wishlist.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { createContext } from "react";
+
+export let wishlistContext = createContext();
 
 let headers = {
   token: localStorage.getItem("userToken"),
@@ -14,4 +17,34 @@ function addToWishList(productId) {
     )
     .then((res) => res)
     .catch((error) => error);
+}
+
+function getWishList() {
+  return axios
+    .get("https://ecommerce.routemisr.com/api/v1/wishlist", {
+      headers: headers,
+    })
+    .then((res) => res)
+    .catch((error) => error);
+}
+
+function deleteProductFromWishlist(productId) {
+  return axios
+    .delete(
+      `https://ecommerce.routemisr.com/api/v1/${productId}`,
+      { productId: productId },
+      { headers: headers }
+    )
+    .then((res) => res)
+    .catch((error) => error);
+}
+
+export default function WishlishContextProvider(props) {
+  return (
+    <wishlistContext.Provider
+      value={{ addToWishList, deleteProductFromWishlist, getWishList }}
+    >
+      {props.children}
+    </wishlistContext.Provider>
+  );
 }
